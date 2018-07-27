0dba13097db5a2662c330e6cac7c271d7062383e
Siarhei Plistniou <siarhei_pliatniou@epam.com>
2018-07-27 18:53:26 +0300

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var protractor = require("gulp-protractor").protractor;
+
+gulp.task("start", () => {
+    return gulp.src([])
+        .pipe(protractor({
+            configFile: "./test/config/conf.js"
+        }))
+        .on('error', (er) => { throw er; })
+}
+);
