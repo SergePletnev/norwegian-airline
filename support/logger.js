8a0940193a0bdfcd70844a440eead277a2e6ee82
Siarhei_Pliatniou <siarhei_pliatniou@epam.com>
2018-11-01 15:46:57 +0300

    fixed logger

--- support/logger.js
+++ support/logger.js
@@ -1,25 +1,55 @@
-const winston = require('winston');
+// const winston = require('winston');
+
+// const path = require('path');
+
+// const logger = winston.createLogger({
+//     transports: [
+//         new (winston.transports.File)({
+//             filename: path.resolve('./logs/combined.log'),
+//             timestamp: () => {
+//                 let currentMoment = new Date(Date.now());
+//                 return currentMoment.toLocaleString('ru', { timeZone: 'Europe/Minsk' });
+//             }
+//         }),
+//         new (winston.transports.File)({
+//             name: 'error-log',
+//             filename: path.resolve('./logs/error.log'),
+//             level: 'error'
+//         }),
+//         new (winston.transports.Console)({
+//             colorize: true
+//         })
+//     ]
+// });
+
+// module.exports = { logger };
 
 const path = require('path');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, printf } = format;
 
-const logger = winston.createLogger({
-    transports: [
-        new (winston.transports.File)({
-            filename: path.resolve('./logs/combined.log'),
-            timestamp: () => {
-                let currentMoment = new Date(Date.now());
-                return currentMoment.toLocaleString('ru', { timeZone: 'Europe/Minsk' });
-            }
+const myFormat = printf(info => {
+    return `${info.timestamp} ${info.message}`;
+});
+
+module.exports = createLogger({
+    format: combine(
+        timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
         }),
-        new (winston.transports.File)({
+        myFormat
+    ),
+    transports: [
+        new (transports.File)({
+            filename: path.resolve('./logs/combined.log')
+        }),
+        new (transports.File)({
             name: 'error-log',
             filename: path.resolve('./logs/error.log'),
             level: 'error'
         }),
-        new (winston.transports.Console)({
+        new (transports.Console)({
             colorize: true
         })
     ]
 });
-
-module.exports = { logger };
